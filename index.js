72665fd1bb503405e4bb216c581348dc54135da8
JacksonSmith43 <jessica444@gmx.at>
2023-09-27 20:06:55 +0200

    Initial commit.

---
+++ index.js
@@ -0,0 +1,34 @@
+const button = document.getElementById("add");
+button.addEventListener("click", handleClick);
+
+const input = document.getElementById("text");
+input.addEventListener("keydown", handleKeyDown);
+
+
+function handleClick() {
+    add();
+}
+
+
+function handleKeyDown(event) {
+    if (event.key === "Enter") {
+        add();
+    }
+}
+
+
+function add() {
+    const input = document.getElementById("text");
+    const note = input.value;
+
+    if (note) {
+        const list = document.getElementById("list");
+        const item = document.createElement("li");
+        item.textContent = note;
+        list.appendChild(item);
+
+        input.value = "";
+        input.focus(); // Goes back to the input field.
+    }
+
+}
