65da7beea43f6a2b3924594fae0bd88b8110f0bd
JacksonSmith43 <jessica444@gmx.at>
2023-09-28 11:51:00 +0200

    Added the add and delete function only to change it all.

--- index.js
+++ index.js
@@ -1,34 +1,71 @@
-const button = document.getElementById("add");
-button.addEventListener("click", handleClick);
+document.addEventListener("DOMContentLoaded", function () {
 
-const input = document.getElementById("text");
-input.addEventListener("keydown", handleKeyDown);
+    const button = document.getElementById("add");
+    button.addEventListener("click", handleClick);
 
+    const input = document.getElementById("text");
+    input.addEventListener("keydown", handleKeyDown);
 
-function handleClick() {
-    add();
-}
+    function handleClickLIItem(event) {
+        const list = document.getElementById("list");
+        list.removeChild(event.target);
+    }
 
-
-function handleKeyDown(event) {
-    if (event.key === "Enter") {
+    function handleClick() {
         add();
     }
-}
 
 
-function add() {
-    const input = document.getElementById("text");
-    const note = input.value;
-
-    if (note) {
-        const list = document.getElementById("list");
-        const item = document.createElement("li");
-        item.textContent = note;
-        list.appendChild(item);
-
-        input.value = "";
-        input.focus(); // Goes back to the input field.
+    function handleKeyDown(event) {
+        if (event.key === "Enter") {
+            add();
+        }
     }
 
-}
+
+    function add() {
+        const input = document.getElementById("text");
+        const note = input.value;
+
+        if (note) {
+            const list = document.getElementById("list");
+            const item = buildLIItem(note);
+            list.appendChild(item);
+            notes.push(note);
+
+            input.value = "";
+            input.focus(); // Goes back to the input field.
+        }
+
+    }
+
+    const notes = [
+        // { id: "676c9ba771", title: "Title 1", text: "ToDo 1" },
+        // { id: "dc191538f", title: "Title 2", text: "ToDo 2" }
+    ];
+
+    function buildLIItem(note) {
+        const item = document.createElement("li");
+        item.id = note.id;
+
+        const article = document.createElement("article");
+        const title = document.createElement("header");
+        title.textContent = note.title;
+        const text = document.createElement("p");
+        text.content = note.text;
+
+        const controls = document.createElement("div");
+        const button = document.createElement("button");
+        button.textContent = "Delete";
+        button.addEventListener("click", handleClickDelete(note.id));
+
+        controls.appendChild(button);
+        article.appendChild(title);
+        article.appendChild(text);
+        article.appendChild(controls);
+        item.appendChild(article);
+
+        return item;
+    }
+
+});
